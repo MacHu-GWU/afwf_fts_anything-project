5e7ea0784901d840081e2067f5bf21e4fa6b9350
Sanhe Hu <husanhe@gmail.com>
2023-04-03 16:11:45 -0400

    fix cov test

--- tests/handlers/test_handler_fts.py
+++ tests/handlers/test_handler_fts.py
@@ -1,16 +1,51 @@
 # -*- coding: utf-8 -*-
+import pytest
 
+from afwf_fts_anything.exc import BuildIndexError
 from afwf_fts_anything.paths import path_settings, path_data, dir_index, dir_icon
+from afwf_fts_anything.dataset import Dataset
 from afwf_fts_anything.handlers.fts import handler
 from rich import print as rprint
 
 class TestHandler:
-    def test(self):
+    def test_build_index(self):
+        dataset = Dataset(
+            name="movie",
+            path_setting=path_settings,
+            path_data=path_data,
+            dir_index=dir_index,
+            dir_icon=dir_icon,
+        )
+        handler.build_index(dataset)
+
+        dataset = Dataset(
+            name="not-exists",
+            path_setting=path_settings,
+            path_data=path_data.change("not-exists-data.json"),
+            dir_index=dir_index.change(new_basename="not-exists"),
+        )
+        with pytest.raises(BuildIndexError):
+            handler.build_index(dataset)
+
+    def test_parse_query(self):
+        assert handler.parse_query("movie ") == dict(dataset_name="movie", query_str="")
+        assert handler.parse_query("movie   ") == dict(dataset_name="movie", query_str="")
+        assert handler.parse_query("movie   ?") == dict(dataset_name="movie", query_str="?")
+        assert handler.parse_query("movie   hello   world") == dict(dataset_name="movie", query_str="hello world")
+        assert handler.parse_query("movie   hello , world") == dict(dataset_name="movie", query_str="hello world")
+
+    def test_main(self):
         sf = handler.main(
             dataset_name="movie",
             query_str="",
         )
         assert sf.items[0].title == "Full text search 'movie' dataset"
+
+        sf = handler.main(
+            dataset_name="movie",
+            query_str="?",
+        )
+        assert sf.items[0].title == "Open 'movie' dataset folder location"
 
         sf = handler.main(
             dataset_name="movie",
