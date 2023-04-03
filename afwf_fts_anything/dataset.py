63dd8596592f61e9d1ca06bc0a41e78bf880d3bd
Sanhe Hu <husanhe@gmail.com>
2023-04-03 13:50:23 -0400

    test dataset

--- afwf_fts_anything/dataset.py
+++ afwf_fts_anything/dataset.py
@@ -1,10 +1,20 @@
 # -*- coding: utf-8 -*-
 
+import typing as T
+import os
+import json
+
 import attr
 from attrs_mate import AttrsClass
 from pathlib_mate import Path
+import requests
+from whoosh import fields, qparser, query, sorting
+from whoosh.index import open_dir, create_in, FileIndex
 
-from .paths import dir_project_home
+from .paths import dir_project_home, dir_cache
+from .compat import cached_property
+from .cache import cache
+from .setting import Setting
 
 
 @attr.s
@@ -23,16 +33,175 @@ class Dataset(AttrsClass):
     - ``${name}-whoosh_index``: the index directory, which contains the whoosh index of this dataset.
         the folder is automatically generated based on your setting and data.
     """
+
     name: str = AttrsClass.ib_str()
+    path_setting: T.Optional[Path] = AttrsClass.ib_generic(
+        type_=Path, nullable=True, default=None
+    )
+    path_data: T.Optional[Path] = AttrsClass.ib_generic(
+        type_=Path, nullable=True, default=None
+    )
+    dir_index: T.Optional[Path] = AttrsClass.ib_generic(
+        type_=Path, nullable=True, default=None
+    )
 
     @property
-    def path_setting(self) -> Path:
+    def _path_setting(self) -> Path:
+        """
+        The path to the setting file.
+        """
+        if self.path_setting is not None:
+            return self.path_setting
         return dir_project_home / f"{self.name}-setting.json"
 
     @property
-    def path_data(self) -> Path:
+    def _path_data(self) -> Path:
+        """
+        The path to the data file.
+        """
+        if self.path_data is not None:
+            return self.path_data
         return dir_project_home / f"{self.name}-data.json"
 
     @property
-    def dir_index(self) -> Path:
+    def _dir_index(self) -> Path:
+        """
+        The path to the whoosh index directory.
+        """
+        if self.dir_index is not None:
+            return self.dir_index
         return dir_project_home / f"{self.name}-whoosh_index"
+
+    @cached_property
+    def setting(self) -> Setting:
+        """
+        Access the setting object that is parsed from the setting file.
+        """
+        return Setting.from_json_file(self._path_setting)
+
+    @cached_property
+    def schema(self) -> fields.Schema:
+        """
+        Access the whoosh schema based on the setting.
+        """
+        return self.setting.create_whoosh_schema()
+
+    def download_data(self): # pragma: no cover
+        """
+        Download the data from the internet if
+        """
+        if self.setting.data_url is None:
+            raise ValueError(
+                "You cannot download data because 'data_url' "
+                f"is not defined in the setting file '{self._path_setting}'."
+            )
+        response = requests.get(self.setting.data_url)
+
+        # write to temp file first, then move to the data file for atomic write
+        path_temp = Path(str(self._path_data) + ".temp")
+        path_temp.write_text(response.text)
+        path_temp.moveto(new_abspath=self._path_data, overwrite=True)
+
+    def get_data(self) -> T.List[T.Dict[str, T.Any]]:
+        """
+        Get the data from the data file. If data file does not exist, download it first.
+        """
+        if not self._path_data.exists(): # pragma: no cover
+            self.download_data()
+        return json.loads(self._path_data.read_text())
+
+    def get_index(self) -> FileIndex:
+        if self._dir_index.exists():
+            idx = open_dir(self._dir_index.abspath)
+        else:
+            self._dir_index.mkdir(parents=True, exist_ok=True)
+            idx = create_in(dirname=self._dir_index.abspath, schema=self.schema)
+        return idx
+
+    def remove_index(self):
+        """
+        Remove the whoosh index diretory.
+        """
+        self._dir_index.remove_if_exists()
+
+    def build_index(self, multi_thread: bool = False, rebuild: bool = False):
+        if rebuild is True:
+            self.remove_index()
+        idx = self.get_index()
+        if multi_thread:  # pragma: no cover
+            writer = idx.writer(procs=os.cpu_count())
+        else:
+            writer = idx.writer()
+
+        for row in self.get_data():
+            doc = {
+                field_name: row.get(field_name)
+                for field_name in self.setting.field_names
+            }
+            writer.add_document(**doc)
+        writer.commit()
+
+    def clear_cache(self): # pragma: no cover
+        dir_cache.remove_if_exists()
+
+    @cache.memoize(expire=5)
+    def search(self, query_str, limit=20) -> T.List[dict]:
+        """
+        Use full-text search for result.
+        """
+        idx = self.get_index()
+        q = query.And(
+            [
+                qparser.MultifieldParser(
+                    self.setting.searchable_fields,
+                    schema=self.schema,
+                ).parse(query_str),
+            ]
+        )
+        multi_facet = sorting.MultiFacet()
+        for field_name in self.setting.sortable_fields:
+            field = self.setting.fields_mapper[field_name]
+            multi_facet.add_field(field_name, reverse=not field.is_sort_ascending)
+        with idx.searcher() as searcher:
+            doc_list = [
+                hit.fields()
+                for hit in searcher.search(
+                    q,
+                    sortedby=multi_facet,
+                    limit=limit,
+                )
+            ]
+        return doc_list
+
+    # @property
+    # def alfred_title(self) -> str:
+    #     return self.title.format(**self.data)
+    #
+    # @property
+    # def alfred_subtitle(self) -> T.Optional[str]:
+    #     if self.subtitle is None:
+    #         return None
+    #     return self.subtitle.format(**self.data)
+    #
+    # @property
+    # def alfred_arg(self) -> T.Optional[str]:
+    #     if self.arg is None:
+    #         return None
+    #     return self.arg.format(**self.data)
+    #
+    # @property
+    # def alfred_autocomplete(self) -> T.Optional[str]:
+    #     if self.autocomplete is None:
+    #         return None
+    #     return self.autocomplete.format(**self.data)
+    #
+    # def to_item(self) -> afwf.Item:
+    #     item = afwf.Item(
+    #         title=self.alfred_title,
+    #         subtitle=self.alfred_subtitle,
+    #         arg=self.alfred_arg,
+    #         autocomplete=self.alfred_autocomplete,
+    #     )
+    #     if self.icon:
+    #         item.set_icon(self.icon)
+    #     return item
--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -1,15 +1,32 @@
 # -*- coding: utf-8 -*-
 
-
+from afwf_fts_anything.paths import path_settings, path_data, dir_index
 from afwf_fts_anything.dataset import Dataset
 
 
 class TestDataset:
-    def test(self):
+    def test_property(self):
         dataset = Dataset(name="movie")
-        assert dataset.path_data.basename == "movie-data.json"
-        assert dataset.path_setting.basename == "movie-setting.json"
-        assert dataset.dir_index.basename == "movie-whoosh_index"
+        assert dataset._path_data.basename == "movie-data.json"
+        assert dataset._path_setting.basename == "movie-setting.json"
+        assert dataset._dir_index.basename == "movie-whoosh_index"
+
+    def test_indexing(self):
+        dataset = Dataset(
+            name="movie",
+            path_setting=path_settings,
+            path_data=path_data,
+            dir_index=dir_index,
+        )
+        dataset.build_index(rebuild=True)
+
+        # cache should work
+        for _ in range(3):
+            doc_list = dataset.search("god father")
+            assert doc_list[0]["movie_id"] == 2
+
+        doc_list = dataset.search("drama", limit=3)
+        assert [doc["movie_id"] for doc in doc_list] == [1, 2, 3]
 
 
 if __name__ == "__main__":
